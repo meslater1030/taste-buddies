3c9bd5cf61dd8cdd7ced69e22010775563594fab
johnsonjjew@gmail.com <johnsonjjew@gmail.com>
2015-07-13 16:30:35 -0700

    user profile DBs

--- tastebuddies/models.py
+++ tastebuddies/models.py
@@ -1,7 +1,6 @@
 from sqlalchemy import (
     Table,
     Column,
-    Index,
     Integer,
     Text,
     ForeignKey
@@ -13,7 +12,6 @@ from sqlalchemy.orm import (
     scoped_session,
     sessionmaker,
     relationship,
-    backref
     )
 
 from zope.sqlalchemy import ZopeTransactionExtension
@@ -23,29 +21,64 @@ Base = declarative_base()
 
 
 usertaste_table = Table('user_profile', Base.metadata,
-                        Column('user', Integer, ForeignKey('user.id')),
+                        Column('users', Integer, ForeignKey('users.id')),
                         Column('profile', Integer, ForeignKey('profile.id'))
                         )
 
 
+userdiet_table = Table('user_diet', Base.metadata,
+                       Column('users', Integer, ForeignKey('users.id')),
+                       Column('profile', Integer, ForeignKey('diet.id'))
+                       )
+
+
+usercost_table = Table('user_cost', Base.metadata,
+                       Column('users', Integer, ForeignKey('users.id')),
+                       Column('profile', Integer, ForeignKey('cost.id'))
+                       )
+
+
 class User(Base):
-    __tablename__ = 'user'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True, autoincrement=True)
     username = Column(Text)
     firstname = Column(Text)
     lastname = Column(Text)
     password = Column(Text)
-    food_profile = relationship("Profile", secondary=usertaste_table,
-                                backref="profiles")
+    age = Column(Integer, ForeignKey('agegroup.id'))
+    user_location = Column(Integer, ForeignKey('location.id'))
+    food_profile = relationship('profile', secondary=usertaste_table)
+    diet_restrict = relationship('diet', secondary=userdiet_table)
+    cost_restrict = relationship('cost', secondary=usercost_table)
     restaurants = Column(Text)
     photo = Column(Text)
 
 
 class Profile(Base):
     __tablename__ = 'profile'
-    id = Column(Integer, primary_key=True)
-    Spicy = Column(Text)
-    Sweet = Column(Text)
-    Savory = Column(Text)
-    Bitter = Column(Text)
-    Sour = Column(Text)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    taste = Column(Text)
+
+
+class AgeGroup(Base):
+    __tablename__ = 'agegroup'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    age_group = Column(Text)
+
+
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    city = Column(Text)
+
+
+class Cost(Base):
+    __tablename__ = 'cost'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    cost = Column(Text)
+
+
+class Diet(Base):
+    __tablename__ = 'diet'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    diet = Column(Text)
