c1b059ff314226721650197dabe34cadaf9bd23b
Megan Slater <megan.e.slater@gmail.com>
2015-07-13 13:10:13 -0700

    updated server to be wherever

--- helloworld.py
+++ helloworld.py
@@ -11,5 +11,5 @@ def app(environ, start_response):
 
 if __name__ == '__main__':
     from wsgiref.simple_server import make_server
-    srv = make_server('localhost', 8080, app)
+    srv = make_server('', 8080, app)
     srv.serve_forever()
