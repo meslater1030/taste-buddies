579c373514882eb971c421433ce745df8405cba5
Megan Slater <megan.e.slater@gmail.com>
2015-07-14 10:59:42 -0700

    test for user creation and db setup

--- tastebuddies/tests.py
+++ tastebuddies/tests.py
@@ -1,11 +1,40 @@
-import unittest
-import transaction
-import pytest
-
-from pyramid import testing
-
-from models import DBSession
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 import models
+import os
+import pytest
+from sqlalchemy import create_engine
+
+TEST_DATABASES_URL = os.environ.get(
+    'DATABASE_URL',
+    'postgresql://meslater:@localhost:5432/test-taste-buddies'
+)
+os.environ['DATABASE_URL'] = TEST_DATABASES_URL
+
+
+# you will want to make a fixture that creates an app
+
+@pytest.fixture(scope='session')
+def connection(request):
+    engine = create_engine(TEST_DATABASES_URL)
+    models.Base.metadata.create_all(engine)
+    connection = engine.connect()
+    models.DBSession.registry.clear()
+    models.DBSession.configure(bind=connection)
+    models.Base.metadata.bind = engine
+    request.addfinalizer(models.Base.metadata.drop_all)
+    return connection
+
+
+@pytest.fixture()
+def db_session(request, connection):
+    from transaction import abort
+    trans = connection.begin()
+    request.addfinalizer(trans.rollback)
+    request.addfinalizer(abort)
+
+    from models import DBSession
+    return DBSession
 
 
 @pytest.fixture()
@@ -37,50 +66,55 @@ def create_user(db_session):
     return user, profile, age, location, cost, diet
 
 
-class TestMyViewSuccessCondition(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
-        Base.metadata.create_all(engine)
-        with transaction.manager:
-            model = MyModel(name='one', value=55)
-            DBSession.add(model)
-
-    def tearDown(self):
-        DBSession.remove()
-        testing.tearDown()
-
-    def test_passing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'tastebuddies')
+def test_create_user(create_user, db_session):
+    create_user
+    assert len(db_session.query('User').all()) == 1
 
 
-class TestMyViewFailureCondition(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp()
-        from sqlalchemy import create_engine
-        engine = create_engine('sqlite://')
-        from .models import (
-            Base,
-            MyModel,
-            )
-        DBSession.configure(bind=engine)
+# class TestMyViewSuccessCondition(unittest.TestCase):
+#     def setUp(self):
+#         self.config = testing.setUp()
+#         from sqlalchemy import create_engine
+#         engine = create_engine('sqlite://')
+#         from .models import (
+#             Base,
+#             MyModel,
+#             )
+#         DBSession.configure(bind=engine)
+#         Base.metadata.create_all(engine)
+#         with transaction.manager:
+#             model = MyModel(name='one', value=55)
+#             DBSession.add(model)
 
-    def tearDown(self):
-        DBSession.remove()
-        testing.tearDown()
+#     def tearDown(self):
+#         DBSession.remove()
+#         testing.tearDown()
 
-    def test_failing_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info.status_int, 500)
+#     def test_passing_view(self):
+#         from .views import my_view
+#         request = testing.DummyRequest()
+#         info = my_view(request)
+#         self.assertEqual(info['one'].name, 'one')
+#         self.assertEqual(info['project'], 'tastebuddies')
+
+
+# class TestMyViewFailureCondition(unittest.TestCase):
+#     def setUp(self):
+#         self.config = testing.setUp()
+#         from sqlalchemy import create_engine
+#         engine = create_engine('sqlite://')
+#         from .models import (
+#             Base,
+#             MyModel,
+#             )
+#         DBSession.configure(bind=engine)
+
+#     def tearDown(self):
+#         DBSession.remove()
+#         testing.tearDown()
+
+#     def test_failing_view(self):
+#         from .views import my_view
+#         request = testing.DummyRequest()
+#         info = my_view(request)
+#         self.assertEqual(info.status_int, 500)
